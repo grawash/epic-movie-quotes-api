9f09f494e7e12a1d4b6a4032f0b34995c5152278
zurbash <duta1101@bk.ru>
2022-11-15 18:28:09 +0400

    feat: implemented jwt authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+	public function __construct()
+	{
+		$this->middleware('auth:api', ['except' => ['login', 'register']]);
+	}
+
+	public function register(StoreUserRequest $request)
+	{
+		$user = User::create($request->validated());
+		auth()->login($user);
+		return response()->json('User successfuly registered!', 200);
+	}
+
+	public function login(LoginRequest $request)
+	{
+		$token = auth()->attempt($request->all());
+		if (!$token)
+		{
+			return response()->json([
+				'status'  => 'error',
+				'message' => 'Unauthorized',
+			], 401);
+		}
+		$user = auth()->user();
+		return response()->json([
+			'status'        => 'success',
+			'user'          => $user,
+			'authorisation' => [
+				'token' => $token,
+				'type'  => 'bearer',
+			],
+		]);
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
 });
-Route::post('register', [RegisterController::class, 'register']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
