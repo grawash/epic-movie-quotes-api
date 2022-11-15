45a3896d1a91a70f944eeec5458bb3970fb6b748
zurbash <duta1101@bk.ru>
2022-11-15 18:31:25 +0400

    feat: added login/logout functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -40,4 +40,11 @@ class AuthController extends Controller
 			],
 		]);
 	}
+
+	public function logout()
+	{
+		auth()->logout();
+
+		return response()->json(['message' => 'Successfully logged out']);
+	}
 }
