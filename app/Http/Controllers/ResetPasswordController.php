2efcea2c953f309c6f5643f03fc160a4ba10aef3
zurbash <duta1101@bk.ru>
2022-11-26 19:36:42 +0400

    fix(missing parameter): changed URI segment of verify route

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PostResetRequest;
 use App\Http\Requests\ResetPasswordRequest;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
+use Firebase\JWT\JWT;
 
 class ResetPasswordController extends Controller
 {
@@ -35,7 +38,17 @@ class ResetPasswordController extends Controller
 				event(new PasswordReset($user));
 			}
 		);
-		return response()->json($status, 201);
+		$user = User::where('email', $request->email)->first();
+		$payload = [
+			'exp' => Carbon::now()->addMinutes(30)->timestamp,
+			'uid' => $user->id,
+		];
+
+		$jwt = JWT::encode($payload, config('auth.jwt_secret'), 'HS256');
+
+		$cookie = cookie('access_token', $jwt, 30, '/', config('auth.front_end_top_level_domain'), true, true, false, 'Strict');
+
+		return response()->json(['success', $jwt], 200)->withCookie($cookie);
 		// return $status === Password::PASSWORD_RESET
 		// 			? redirect()->route('reset.login')->with('status', __($status))
 		// 			: back()->withErrors(['email' => [__($status)]]);
